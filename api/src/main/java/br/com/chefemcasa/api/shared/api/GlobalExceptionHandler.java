c6bf223ec981e186a0b4f7dec7d2738199169599
Ítalo Vitorino <italo.vitorinobs@hotmail.com>
2026-05-11 00:45:15 -0300

    feat(solicitations): adicionar handlers de exceções ao GlobalExceptionHandler

--- api/src/main/java/br/com/chefemcasa/api/shared/api/GlobalExceptionHandler.java
+++ api/src/main/java/br/com/chefemcasa/api/shared/api/GlobalExceptionHandler.java
@@ -3,6 +3,9 @@ package br.com.chefemcasa.api.shared.api;
 import br.com.chefemcasa.api.identity.domain.exception.EmailAlreadyRegisteredException;
 import br.com.chefemcasa.api.identity.domain.exception.InvalidCredentialsException;
 import br.com.chefemcasa.api.identity.domain.exception.UserNotFoundException;
+import br.com.chefemcasa.api.solicitations.domain.exception.InvalidTransitionException;
+import br.com.chefemcasa.api.solicitations.domain.exception.SolicitationNotFoundException;
+import br.com.chefemcasa.api.solicitations.domain.exception.UnauthorizedActorException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
@@ -32,5 +35,26 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         var pd = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
         pd.setTitle("Não encontrado");
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(pd);
+    }
+
+    @ExceptionHandler(SolicitationNotFoundException.class)
+    public ResponseEntity<ProblemDetail> handleSolicitationNotFound(SolicitationNotFoundException ex) {
+        var pd = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+        pd.setTitle("Não encontrado");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(pd);
+    }
+
+    @ExceptionHandler(InvalidTransitionException.class)
+    public ResponseEntity<ProblemDetail> handleInvalidTransition(InvalidTransitionException ex) {
+        var pd = ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.getMessage());
+        pd.setTitle("Transição inválida");
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(pd);
+    }
+
+    @ExceptionHandler(UnauthorizedActorException.class)
+    public ResponseEntity<ProblemDetail> handleUnauthorizedActor(UnauthorizedActorException ex) {
+        var pd = ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, ex.getMessage());
+        pd.setTitle("Acesso negado");
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(pd);
     }
 }
