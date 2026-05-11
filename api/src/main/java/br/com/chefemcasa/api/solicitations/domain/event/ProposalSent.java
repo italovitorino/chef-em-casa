d6e71bc223d46ef262b856289b65b8996d441a61
Ítalo Vitorino <italo.vitorinobs@hotmail.com>
2026-05-11 00:09:37 -0300

    feat(solicitations): adicionar eventos de domínio

---
+++ api/src/main/java/br/com/chefemcasa/api/solicitations/domain/event/ProposalSent.java
@@ -0,0 +1,12 @@
+package br.com.chefemcasa.api.solicitations.domain.event;
+
+import br.com.chefemcasa.api.shared.domain.event.DomainEvent;
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+public record ProposalSent(
+        UUID eventId, Instant occurredAt, int version,
+        UUID solicitationId, UUID clientId, UUID chefId,
+        BigDecimal totalAmount
+) implements DomainEvent {}
